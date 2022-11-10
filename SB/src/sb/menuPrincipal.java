1e88f077fcf6ea7f1f70291e5ca96c567ece4141
Anhonycd13 <72539266+Anhonycd13@users.noreply.github.com>
2022-11-09 23:34:24 -0600

    091122
    talba transsacion

--- SB/src/sb/menuPrincipal.java
+++ SB/src/sb/menuPrincipal.java
@@ -39,10 +39,7 @@ public class menuPrincipal extends javax.swing.JFrame {
         saveAsMenuItem = new javax.swing.JMenuItem();
         exitMenuItem = new javax.swing.JMenuItem();
         editMenu = new javax.swing.JMenu();
-        cutMenuItem = new javax.swing.JMenuItem();
-        copyMenuItem = new javax.swing.JMenuItem();
-        pasteMenuItem = new javax.swing.JMenuItem();
-        deleteMenuItem = new javax.swing.JMenuItem();
+        itemdeposito = new javax.swing.JMenuItem();
         jMenu1 = new javax.swing.JMenu();
         item_cliente = new javax.swing.JMenuItem();
         itemsucursal = new javax.swing.JMenuItem();
@@ -80,23 +77,15 @@ public class menuPrincipal extends javax.swing.JFrame {
         menuBar.add(fileMenu);
 
         editMenu.setMnemonic('e');
-        editMenu.setText("Editar");
+        editMenu.setText("Transacción");
 
-        cutMenuItem.setMnemonic('t');
-        cutMenuItem.setText("Cut");
-        editMenu.add(cutMenuItem);
-
-        copyMenuItem.setMnemonic('y');
-        copyMenuItem.setText("Copy");
-        editMenu.add(copyMenuItem);
-
-        pasteMenuItem.setMnemonic('p');
-        pasteMenuItem.setText("Paste");
-        editMenu.add(pasteMenuItem);
-
-        deleteMenuItem.setMnemonic('d');
-        deleteMenuItem.setText("Delete");
-        editMenu.add(deleteMenuItem);
+        itemdeposito.setText("Deposito");
+        itemdeposito.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                itemdepositoActionPerformed(evt);
+            }
+        });
+        editMenu.add(itemdeposito);
 
         menuBar.add(editMenu);
 
@@ -189,6 +178,26 @@ public class menuPrincipal extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_itemsucursalActionPerformed
 
+    private void itemdepositoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_itemdepositoActionPerformed
+        
+        String x = transaccion.x;
+        
+        try {
+            if(x==null){
+            transaccion trans = new transaccion();
+            desktopPane.add(trans);
+            desktopPane.moveToFront(trans);
+            }
+            else{
+                JOptionPane.showMessageDialog(rootPane, "La venta ya esta en uso");
+            }
+            
+        } catch(Exception e){
+        e.printStackTrace();
+        }
+        
+    }//GEN-LAST:event_itemdepositoActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -227,20 +236,17 @@ public class menuPrincipal extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem aboutMenuItem;
     private javax.swing.JMenuItem contentMenuItem;
-    private javax.swing.JMenuItem copyMenuItem;
-    private javax.swing.JMenuItem cutMenuItem;
-    private javax.swing.JMenuItem deleteMenuItem;
     public static javax.swing.JDesktopPane desktopPane;
     private javax.swing.JMenu editMenu;
     private javax.swing.JMenuItem exitMenuItem;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenu helpMenu;
     private javax.swing.JMenuItem item_cliente;
+    private javax.swing.JMenuItem itemdeposito;
     private javax.swing.JMenuItem itemsucursal;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar menuBar;
     private javax.swing.JMenuItem openMenuItem;
-    private javax.swing.JMenuItem pasteMenuItem;
     private javax.swing.JMenuItem saveAsMenuItem;
     private javax.swing.JMenuItem saveMenuItem;
     // End of variables declaration//GEN-END:variables
